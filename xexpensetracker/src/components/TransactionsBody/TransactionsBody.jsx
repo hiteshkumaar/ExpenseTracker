60ade976652d57a5fbc83be8da639d35dbca58d6
Hitesh Kumaar <uiintern.smartail@gmail.com>
2025-06-23 10:27:43 +0530

    Refactor TransactionsBody to remove pagination and display all transactions

--- xexpensetracker/src/components/TransactionsBody/TransactionsBody.jsx
+++ xexpensetracker/src/components/TransactionsBody/TransactionsBody.jsx
@@ -10,54 +10,34 @@ import { TransactionsContext } from '../../Contexts/AllContexts';
 const TransactionsBody = () => {
     //contexts
     const [transactionData, setTransactionData] = useContext(TransactionsContext);
-    //states
-    const [pages, setPages] = useState({ currentPage: 1, totalPages: 1 })
-    //everytime transactionData updates
-    useEffect(()=> {
-        onLoad();
-    }, [transactionData])
+
     //functions
     const displayTransactions = () => {
         let key = 0;
-        if(transactionData && transactionData.length){
-            let arr =[];
-            let startIndex = 5 * (pages.currentPage - 1)
-            let endIndex = (5 * pages.currentPage) - 1
+        if (transactionData && transactionData.length) {
+            return transactionData.map(({ name, date, price, category, id }) => (
+                <TransactionBar
+                    key={`${key++}`}
+                    name={name}
+                    date={date}
+                    amount={price}
+                    category={category}
+                    id={id}
+                />
+            ));
+        }
+        return null;
+    };
 
-            for(let i = startIndex; i <= endIndex; i++){
-                if(i >= transactionData.length) break;
-                const { name, date, price, category, id } = transactionData[i];
-                arr.push(
-                    <TransactionBar key={`${key++}`} name={name} date={date} amount={price} category={category} id={id}/>
-                )
-            }
-
-            return arr;
-        }
-    }
-    const onLoad = () =>{
-        setPages({ currentPage: 1, totalPages: Math.ceil(transactionData.length / 5) })
-    }
-    
-    const updatePage = direction => {
-        let {currentPage, totalPages} = pages;
-        if(direction === "right" && currentPage < totalPages){
-            setPages({...pages, currentPage: currentPage+1})
-        }
-        if(direction === "left" && currentPage > 1){
-            setPages({...pages, currentPage: currentPage-1})
-        }
-    }
     return (
         <div className='TransactionBody'>
             <div className='transactionBodyUpper'>
                 <div className='transactionPage'>{displayTransactions()}</div>
             </div>
-            <div className='transactionBodylower'>
-                <PageNavigateBar key={"pageNavigate"} pages={pages} updatePage={updatePage} />
-            </div>
+            {/* Pagination removed since we're showing all transactions */}
         </div>
     );
 };
+
 
 export default TransactionsBody;
